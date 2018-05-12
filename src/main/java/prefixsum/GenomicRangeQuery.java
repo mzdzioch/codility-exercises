bfd452357ee8e1275f0a07d9f90a291624613856
mzdzioch <michal.zdziohc@gmail.com>
2018-05-12 20:28:35 +0200

    Added GenomicRangeQuery

--- src/main/java/prefixsum/GenomicRangeQuery.java
+++ src/main/java/prefixsum/GenomicRangeQuery.java
@@ -1,4 +1,39 @@
 package prefixsum;
 
 public class GenomicRangeQuery {
+    public int[] solution(String S, int[] P, int[] Q){
+        int length = P.length;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            result[i] = findMinmalFactor(S, P[i], Q[i]);
+
+        }
+
+        return result;
+    }
+
+    private int findMinmalFactor(String s, int a, int b) {
+
+        char[] chars = s.toCharArray();
+        int min = 0;
+
+        for (int i = a; i <= b; i++) {
+            min = Math.min(min, getFactor(chars[i]));
+        }
+
+        return min;
+    }
+
+    public int getFactor(char a){
+        switch (a){
+            case 'A': return 1;
+            case 'C': return 2;
+            case 'G': return 3;
+            case 'T': return 4;
+            default: return -1;
+        }
+    }
+
+
 }
--- src/test/java/prefixsum/GenomicRangeQueryTest.java
+++ src/test/java/prefixsum/GenomicRangeQueryTest.java
@@ -1,4 +1,20 @@
 package prefixsum;
 
+import org.junit.Test;
+
 public class GenomicRangeQueryTest {
+
+    @Test
+    public void shouldReturnArrayOfMinimalImpactFactor(){
+
+        int[] P = {2, 5, 0};
+        int[] Q = {4, 5, 6};
+        String s = "CAGCCTA";
+
+        int[] expectedResult = {2, 4, 1};
+        GenomicRangeQuery genomicRangeQuery = new GenomicRangeQuery();
+        int[] result = genomicRangeQuery.solution(s, P, Q);
+
+
+    }
 }
